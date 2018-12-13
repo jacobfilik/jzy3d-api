259eaa589664ee478d29460068019737532f488f
Jacob Filik <jacob.filik@diamond.ac.uk>
2018-12-13 20:31:45 +0000

    Demo using lizard volume

--- jzy3d-tutorials/src/main/java/org/jzy3d/demos/incubation/VolumeRenderDemo.java
+++ jzy3d-tutorials/src/main/java/org/jzy3d/demos/incubation/VolumeRenderDemo.java
@@ -1,5 +1,7 @@
 package org.jzy3d.demos.incubation;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import org.jzy3d.analysis.AbstractAnalysis;
@@ -7,11 +9,17 @@ import org.jzy3d.analysis.AnalysisLauncher;
 import org.jzy3d.chart.factories.AWTChartComponentFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.colors.ColorMapper;
+import org.jzy3d.colors.colormaps.ColorMapGrayscale;
 import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.incubation.volume.Texture3D;
+import org.jzy3d.io.matlab.MatlabVBOLoader;
 import org.jzy3d.maths.BoundingBox3d;
+import org.jzy3d.plot3d.primitives.axes.layout.IAxeLayout;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
 
+import com.jmatio.io.MatFileReader;
+import com.jmatio.types.MLArray;
+import com.jmatio.types.MLNumericArray;
 import com.jogamp.opengl.util.GLBuffers;
 
 public class VolumeRenderDemo extends AbstractAnalysis {
@@ -22,23 +30,53 @@ public class VolumeRenderDemo extends AbstractAnalysis {
     @Override
     public void init() {
         
-        ColorMapper colorMapper = new ColorMapper(new ColorMapRainbow(), 0, 1, new Color(1, 1, 1, .5f));
-
-        ByteBuffer buffer = GLBuffers.newDirectByteBuffer(10*10*10*4);
-		//make some kind of volume
-		for (float x = 0; x < 2; x+=0.2) {
-			for (float y = 0; y < 2; y+=0.2) {
-				for (float z = 0; z < 2; z+=0.2) {
-					buffer.putFloat((float)Math.sin(x*y*z));
+    	ByteBuffer buffer = null;
+    	int[] shape = null;
+    	float max = Float.NEGATIVE_INFINITY;
+    	float min = Float.POSITIVE_INFINITY;
+    	
+    	 try {
+			MatFileReader mfr = new MatFileReader("C:\\data\\lizard.mat");
+			MLNumericArray<Integer> data = (MLNumericArray<Integer>)mfr.getMLArray("data");
+			shape = data.getDimensions();
+			int size = data.getSize();
+			data.toString();
+			
+			buffer = GLBuffers.newDirectByteBuffer(size*4);
+			
+			for (int i = 0; i < size; i++) {
+				float f = data.get(i).floatValue();
+				buffer.putFloat(f);
+				
+				if (f < min) {
+					min = f;
+				}
+				
+				if (f > max) {
+					max = f;
 				}
 			}
+			
+			
+		} catch (Exception e) {
+			return;
 		}
+    	 
+    	int temp = shape[0];
+    	shape[0] = shape[2];
+    	shape[2] = temp;
+    	
+        ColorMapper colorMapper = new ColorMapper(new ColorMapGrayscale(), min, max, new Color(1, 1, 1, .5f));
 		
-		Texture3D volume = new Texture3D(buffer, new int[] {10,10,10},(float)0,(float)1,colorMapper,new BoundingBox3d(1,10,1,10,1,10));	
+		Texture3D volume = new Texture3D(buffer, shape,(float)min+ ((max-min)/10),(float)max-((max-min)/10),colorMapper,new BoundingBox3d(0,shape[2],0,shape[1],0,shape[0]));	
         
         // Create a chart
         chart = AWTChartComponentFactory.chart(Quality.Intermediate, getCanvasType());
         chart.getScene().getGraph().add(volume);
+//        chart.getView().setBackgroundColor(new Color(0, 0, 0));
+//        IAxeLayout axeLayout = chart.getAxeLayout();
+//        axeLayout.setMainColor(new Color(0.7f, 0.7f, 0.7f));
+        chart.getView().setSquared(false);
         chart.getView();
     }
 }
